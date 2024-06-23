98669040e5e363b92e5c4601534cb4c37efb839c
XINUJ <159104110+XINUJ@users.noreply.github.com>
2024-06-24 00:54:09 +0800

    Initial commit

---
+++ resources/js/components/forms/login.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.email"
+          type="email"
+          placeholder="Enter email"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Password:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.password"
+          type="password"
+          placeholder="Enter Your Password"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-4" v-slot="{ ariaDescribedby }">
+        <b-form-checkbox-group
+          v-model="form.checked"
+          id="checkboxes-4"
+          :aria-describedby="ariaDescribedby"
+        >
+      
+        </b-form-checkbox-group>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary" @click="register()">Submit</b-button>
+    </b-form>
+   
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        form: {
+          email: '',
+          name: '',
+          food: null,
+          checked: []
+        },
+        foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
+        show: true
+      }
+    },
+    methods: {
+       register(){
+        axios.post('/register/registration',this.form).then(response=>{
+        console.log(response.data.email); 
+        console.log(response.data.password);
+        }).catch(error=>{ 
+          console.log(error.data);
+        this.loading = false;
+        })
+      },
+
+      onSubmit(event){
+        event.preventDefault()
+          alert(JSON.stringify(this.form));},
+
+      onReset(event) {
+        event.preventDefault()
+        // Reset our form values
+        this.form.email = ''
+        this.form.name = ''
+        this.form.food = null
+        this.form.checked = []
+        // Trick to reset/clear native browser form validation state
+        this.show = false
+        this.$nextTick(() => {
+          this.show = true
+        })
+      }
+    }
+  }
+</script>
